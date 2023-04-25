b2689bf0de711f4dc70036c6031d9e8db6ab0a72
pierre <jobrider@gmail.com>
2023-04-25 20:18:56 +0200

    reglage bottom avec transparence et ajout header et footer page commander.php

--- wp-content/themes/hello-child/commander.php
+++ wp-content/themes/hello-child/commander.php
@@ -9,7 +9,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $adresse = $_POST["adresse"];
     $cp = $_POST["cp"];
     $ville = $_POST["ville"];
-
+}
+/*
+Template Name: Commander
+*/
+get_header();
     // Vérification des données reçues
    /*  if (empty($nom) || empty($prenom) || empty($email) || empty($adresse) || empty($cp) || empty($ville)) {
         echo "Veuillez remplir tous les champs obligatoires";
@@ -31,6 +35,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             echo "Une erreur est survenue lors de l'envoi de l'e-mail de confirmation.";
         } */
-    }
-
+get_footer();
 ?>
--- wp-content/themes/hello-child/functions.php
+++ wp-content/themes/hello-child/functions.php
@@ -22,3 +22,28 @@ function hello_elementor_child_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );    
 
+/* afficher le menu admin que si user connecté */
+
+function hello_elementor_child_menu_args( $args ) {
+    if ( is_user_logged_in() ) {
+        $args['menu'] = 'mainmenu';
+    } else {
+        $args['menu'] = 'mainmenu-notlogged';
+    }
+    return $args;
+}
+add_filter( 'wp_nav_menu_args', 'hello_elementor_child_menu_args' );
+
+/* afficher un footer diff selon la page */
+/* function isPageCommander( $args ) {
+
+if(is_page( 'commander' )){
+	
+}
+
+
+}
+add_filter( 'wp_nav_menu_args', 'isPageCommander' ); */
+
+?>
+
